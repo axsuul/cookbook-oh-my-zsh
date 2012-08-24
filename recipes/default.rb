d9a2214e363e278de2373de687b8d577b37d6725
James Hu <axsuul@gmail.com>
2012-08-24 01:09:19 -0700

    get rid of data bags

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,25 +20,27 @@
 include_recipe "git"
 include_recipe "zsh"
 
-search( :users, "shell:*zsh" ).each do |u|
-  user_id = u["id"]
-
-  git "/home/#{user_id}/.oh-my-zsh" do
+node[:ohmyzsh][:users].each do |user|
+  git "/home/#{user}/.oh-my-zsh" do
     repository "https://github.com/robbyrussell/oh-my-zsh.git"
     reference "master"
-    user user_id
-    group user_id
+    user user
     action :checkout
-    not_if "test -d /home/#{user_id}/.oh-my-zsh"
+    not_if "test -d /home/#{user}/.oh-my-zsh"
+  end 
+
+  template_variables = { :theme => (node[:ohmyzsh][:theme]) }
+  template_variables[:commands] = node[:ohmyzsh][user.to_sym][:commands] if node[:ohmyzsh][user.to_sym] and node[:ohmyzsh][user.to_sym][:commands]
+
+  template "/home/#{user}/.zshrc" do
+    source "zshrc.erb"
+    owner user
+    variables template_variables
+    action :create
   end
 
-  theme = data_bag_item( "users", user_id )["oh-my-zsh-theme"]
-
-  template "/home/#{user_id}/.zshrc" do
-    source "zshrc.erb"
-    owner user_id
-    group user_id
-    variables( :theme => ( theme || node[:ohmyzsh][:theme] ))
-    action :create_if_missing
+  # set the default shell for user
+  execute "change default shell to zsh" do
+    command "sudo chsh -s /bin/zsh #{user}"
   end
 end
